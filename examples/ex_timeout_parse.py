0933ec414d3b46abb1e9a7a84f53ba9008616d68
Alex <atandy@appnexus.com>
2016-02-22 22:19:03 -0500

    update docs and get RT scores

--- examples/ex_timeout_parse.py
+++ examples/ex_timeout_parse.py
@@ -2,18 +2,27 @@ import requests
 import bs4
 import json
 
-#panels = [2,3,4]
-url = "http://www.timeout.com/newyork/restaurants/100-best-new-york-restaurants#tab_panel_1"
-restaurant_list_of_dicts = []
+panels = [2,3,4]
+url = "http://www.timeout.com/newyork/restaurants/100-best-new-york-restaurants#tab_panel_{}"
 
-requests.get(url.format(url)).content
-soup = bs4.BeautifulSoup(r)
- #TODO: iterate through all panels. 
-for i in soup.findAll('a'):
-	data_tracking = i.get('data-tracking')
-	if data_tracking:
-		dt_json = json.loads(data_tracking)
-		content_name =  dt_json['attributes'].get('contentName')
-		if content_name and content_name.count(' ') < 4:
-			d = {'restaurant_name': content_name }
-			restaurant_list_of_dicts.append(d)
+restaurant_list_of_dicts = []
+for panel in panels:
+	requests.get(url.format(url.format(panel))).content
+	soup = bs4.BeautifulSoup(r)
+	 #TODO: iterate through all panels. 
+	for link in soup.findAll('a'):
+		data_tracking = link.get('data-tracking')
+		if data_tracking:
+			dt_json = json.loads(data_tracking)
+			content_name =  dt_json['attributes'].get('contentName')
+			if content_name and content_name.count(' ') < 4:
+				d = {'restaurant_name': content_name }
+				restaurant_list_of_dicts.append(d)
+
+# write the results to a CSV.
+with open('restaurants.csv', 'w') as csvfile:
+    fieldnames = restaurant_list_of_dicts[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for restaurant in restaurant_list_of_dicts:
+        writer.writerow(restaurant)
--- examples/get_rotten_score.py
+++ examples/get_rotten_score.py
@@ -1,16 +1,17 @@
-#TODO: add correct shebang to files 
-
 import requests
 import time
 import json
 import os, sys
-
+import csv
+from collections import OrderedDict
 
 def get_config(file_path):
+    ''' get the config file and load it as json'''
     with open(file_path) as f: 
         return json.load(f)
 
 def get_rotten_scores(movie_name):
+    ''' get a response from RT API for a movie name as str '''
     movie_url = API_URL.format(ROTTEN_API_KEY,movie_name)
     r = requests.get(movie_url).json()
     return r
@@ -25,18 +26,23 @@ CONFIG = get_config(CONFIG_NAME)
 ROTTEN_API_KEY = CONFIG['api_key']
 API_URL = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey={}&q={}&page_limit=1"
 
+# example list of movies
 movies = [  "While We're Young"," The Rewrite"," Aloha","Saint Laurent",
             "1/3 of A Little Chaos","Life Partners","Fort Tilden","Adult Beginners",
             "House of Cards","Madame Bovary","Face of an Angel","In Your Eyes",
             "The Immigrant"," 1/2 of Laggies"," Barefoot","Dior and I"]
 
+movie_score_dict_list = []
 
 for movie in movies:
     r = get_rotten_scores(movie)
     time.sleep(1)
+    # set empty false values in the event the API returns nothing.
     audience_score = 000
     critic_score = 000
     year = 0000
+
+    # try to get values, pass if they are not found. 
     try:
         audience_score = r['movies'][0]['ratings']['audience_score']
     except:
@@ -49,4 +55,21 @@ for movie in movies:
         year = r['movies'][0].get('year')
     except:
         pass
-    print movie + "| " + str(audience_score) + "| " + str(critic_score) + "| " + str(year)
+
+    d = {
+        'movie_name': movie,
+        'audience_score': audience_score, 
+        'critic_score': critic_score,
+        'year': year
+    }
+    
+    movie_score_dict_list.append(OrderedDict(d))
+    #print movie + "| " + str(audience_score) + "| " + str(critic_score) + "| " + str(year)
+
+# write the results to a CSV.
+with open('movie_scores.csv', 'w') as csvfile:
+    fieldnames = movie_score_dict_list[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for movie_score_dict in movie_score_dict_list:
+        writer.writerow(movie_score_dict)
